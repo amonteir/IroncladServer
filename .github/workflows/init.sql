54b2c668377949ca51835503d79f593ab7c92223
Angelo Monteiro <afrmonteiro@gmail.com>
2023-08-31 11:41:45 +0100

    new CI steps: created init.sql script and added psql db container to ci.yaml

---
+++ .github/workflows/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    pwd VARCHAR(50) NOT NULL
+);
+
+INSERT INTO users (username, pwd) VALUES
+('mock1', 'password1'),
+('mock2', 'password2'),
+('mock3', 'password3');
